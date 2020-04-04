bbeb7f9fe2b8632911016627d774585cf610c2b9
Joseph Rose <joeyrose1999@gmail.com>
2020-04-04 16:11:36 -0700

    added backoff model, punctuation removal, lemmatization, stopword remov
    changed test/train ratio to 25/75

--- dc2.py
+++ dc2.py
@@ -3,7 +3,36 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+from nltk.corpus import stopwords
+import string
 
+lemmatizer = WordNetLemmatizer()
+def nltk2wn_tag(nltk_tag):
+    if nltk_tag.startswith('J'):
+        return wordnet.ADJ
+    elif nltk_tag.startswith('V'):
+        return wordnet.VERB
+    elif nltk_tag.startswith('N'):
+        return wordnet.NOUN
+    elif nltk_tag.startswith('R'):
+        return wordnet.ADV
+    else:          
+        return None
+def lemmatize_sentence(sentence):
+    nltk_tagged = nltk.pos_tag(nltk.word_tokenize(sentence))  
+    wn_tagged = map(lambda x: (x[0], nltk2wn_tag(x[1])), nltk_tagged)
+    res_words = []
+    for word, tag in wn_tagged:
+        if tag is None:            
+            res_words.append(word)
+        else:
+            res_words.append(lemmatizer.lemmatize(word, tag))
+    return " ".join(res_words)
+
+#remaining stopwords after cutting off last 50~ words of "from nltk.corpus import stopwords" since they had sentiment value (don't, should've, etc)
 
 data = pd.read_csv('spam 2.csv', header=None, delimiter=',,,', engine='python') # names=['text', 'label'],
 data = data.drop([0])
@@ -19,10 +48,23 @@ def splitter(x):
     labels.append(label)
 
     new_text = "".join(spl_str[1:len(spl_str)])
-    texts.append(new_text)
+    #remove punctuation
+    nopunc = [char for char in new_text if char not in string.punctuation]
+    nopunc = ''.join(nopunc)
+    #lemmatize
+    new_text = lemmatize_sentence(nopunc)
+        
+    #remove stop words
+    wordList = new_text.split()
+    goodWords = []
+    for word in wordList:
+        if word.lower() not in stopwords.words('english'):
+            goodWords.append(word)
+    texts.append(' '.join(goodWords))
 
-
-df = [splitter(x) for x in data[0]]
+#generate list of 'spam' or 'ham' labels and list of messages, used to create dataframe representing csv file
+for x in data[0]:
+    splitter(x) 
 
 result = pd.DataFrame(texts, columns=['text'])
 label_df = pd.DataFrame(labels, columns=['label'])
@@ -30,54 +72,34 @@ result = result.join(label_df)
 
 print(result.head())
 
-tfidf_vectorizer = TfidfVectorizer(stop_words='english')
-bow_vectorizer = CountVectorizer(stop_words='english') # unigrams
-bigram_vectorizer = CountVectorizer(ngram_range=(2,2), stop_words='english') # bigrams
+tfidf_vectorizer = TfidfVectorizer()
+bow_vectorizer = CountVectorizer() # unigrams
+bigram_vectorizer = CountVectorizer(ngram_range=(2,2)) # bigrams
 model = MultinomialNB()
 
 X = result['text']
 y = result['label']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 # feature extraction
-tfidf_train_feats = tfidf_vectorizer.fit_transform(X_train)
-tfidf_test_feats = tfidf_vectorizer.transform(X_test)
-
 bow_train_feats = bow_vectorizer.fit_transform(X_train)
 bow_test_feats = bow_vectorizer.transform(X_test)
 
 bigram_train_feats = bigram_vectorizer.fit_transform(X_train)
 bigram_test_feats = bigram_vectorizer.transform(X_test)
 
-# testing tfidf
-print('TESTING TFIDF\n')
-print('======================')
-model.fit(tfidf_train_feats, y_train)
-tfidf_pred = model.predict(tfidf_test_feats)
-print('Accuracy score: ', accuracy_score(y_test, tfidf_pred))
-print('Precision score: ', precision_score(y_test, tfidf_pred, pos_label='spam'))
-print('Recall score: ', recall_score(y_test, tfidf_pred, pos_label='spam'))
-print('F-1 score: ', f1_score(y_test, tfidf_pred, pos_label='spam'))
-print(classification_report(y_test, tfidf_pred))
-
-# testing bag of words
-print('TESTING BAG OF WORDS\n')
-print('======================')
 model.fit(bow_train_feats, y_train)
 bow_pred = model.predict(bow_test_feats)
-print('Accuracy score: ', accuracy_score(y_test, bow_pred))
-print('Precision score: ', precision_score(y_test, bow_pred, pos_label='spam'))
-print('Recall score: ', recall_score(y_test, bow_pred, pos_label='spam'))
-print('F-1 score: ', f1_score(y_test, bow_pred, pos_label='spam'))
-print(classification_report(y_test, bow_pred))
 
-# testing bigrams
-print('TESTING BIGRAMS\n')
+# testing backoff
+print('TESTING BACKOFF\n')
 print('======================')
 model.fit(bigram_train_feats, y_train)
 bigram_pred = model.predict(bigram_test_feats)
-print('Accuracy score: ', accuracy_score(y_test, bigram_pred))
-print('Precision score: ', precision_score(y_test, bigram_pred, pos_label='spam'))
-print('Recall score: ', recall_score(y_test, bigram_pred, pos_label='spam'))
-print('F-1 score: ', f1_score(y_test, bigram_pred, pos_label='spam'))
+#find messages classified as 'ham', run BoW on them to see if they're actually 'spam'
+ham_indices = [i for i, value in enumerate(bigram_pred) if value == 'ham']
+#aka use the output in the BoW at those indices
+for index in ham_indices:
+    bigram_pred[index] = bow_pred[index]
+
 print(classification_report(y_test, bigram_pred))
--- test.py
+++
@@ -1,5 +0,0 @@
-# simple file for calculating averages because I got tired typing everything
-import numpy as np
-
-list = [0.9171974522292993, 0.8896797153024911, 0.9083333333333334, 0.9329073482428115, 0.9072164948453608]
-print(np.mean(list))
